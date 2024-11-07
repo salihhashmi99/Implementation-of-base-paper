f35f40823e88f99f08d3cc1defe0bcc62a9bc9f9
Salih Tanveer Hashmi <82093715+salihhashmi99@users.noreply.github.com>
2024-11-06 23:00:33 -0800

    Create datapreprocessing.py

---
+++ datapreprocessing.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import nltk
+import re
+
+# Download necessary resources for nltk (only needed once)
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
